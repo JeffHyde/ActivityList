5b877a15efcbf1c12993a937bd157e85bd443b10
Jeff Hyde <scrapy750@gmail.com>
2018-06-13 12:22:54 -0700

    Star Button constraint changes.

---
+++ ActivityList/Extension/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  ActivityList
+//
+//  Created by Jeff  Hyde on 6/13/18.
+//  Copyright © 2018 Jeff  Hyde. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+extension UIColor {
+    static var mainColor: UIColor {
+        return UIColor(red: 100/255, green: 66/255, blue: 162/255, alpha: 1.0)
+}
+}
